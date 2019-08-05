e186c2d997007085f63981d22ae3bcf4fd3d41bc
gpriaulx <eliette@priaulx.co>
2019-08-05 13:35:39 +0000

    Deploy from AppVeyor

---
+++ interactions.js
@@ -0,0 +1,3 @@
+function backgroundColourGenerator()
+{var candidateBackgroundColours=["#FF1053","#333333","#6C6EA0","#66C7F4","#FC814A","#2292A4","#D96C06","#FE5F55","#749C75","#6A5D7B"];var colourIndex=Math.floor(Math.random()*candidateBackgroundColours.length);var nominatedBackgroundColour=candidateBackgroundColours[colourIndex];document.getElementById('background').style.background=nominatedBackgroundColour;}
+document.addEventListener('DOMContentLoaded',backgroundColourGenerator,false);
